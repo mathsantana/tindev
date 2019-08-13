b69fcd17e98d453b08dc157692653e20fa65ef19
Matheus Santana <matheus.santana@ccc.ufcg.edu.br>
2019-08-13 14:34:18 -0300

    main page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
 import logo from '../assets/logo.svg';
 
-export default function Login() {
+import api from '../services/api.js';
+
+export default function Login({ history }) {
+    const [username, setUsername] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const response = await api.post('/devs', {
+            username
+        });
+
+        history.push(`/dev/${response.data._id}`)
+    }
+
     return (
         <div className='login-container'>
-            <form>
-                    <img src={logo} alt='Logo Tindev'/>
-                <input placeholder='Digite seu usuário do Github' />
+            <form onSubmit={handleSubmit}>
+                <img src={logo} alt='Logo Tindev'/>
+                <input 
+                placeholder='Digite seu usuário do Github'
+                value={username}
+                onChange={e => setUsername(e.target.value)} 
+                />
                 <button type='submit'>Entrar</button>
             </form>
         </div>
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,8 +1,77 @@
 import React from 'react';
+import './Main.css';
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
 
-export default function Main() {
+
+export default function Main({ match }) {
     return (
-        <h1>Hello World</h1>
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/40031356?v=4" alt="" />
+                    <footer>
+                        <strong>Matheus Santana</strong>
+                        <p>Computer Science Student - UFCG</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt='dislike' />
+                        </button>
+                        <button type="button">
+                            <img src={like} alt='like' />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/40031356?v=4" alt="" />
+                    <footer>
+                        <strong>Matheus Santana</strong>
+                        <p>Computer Science Student - UFCG</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt='dislike' />
+                        </button>
+                        <button type="button">
+                            <img src={like} alt='like' />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/40031356?v=4" alt="" />
+                    <footer>
+                        <strong>Matheus Santana</strong>
+                        <p>Computer Science Student - UFCG</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt='dislike' />
+                        </button>
+                        <button type="button">
+                            <img src={like} alt='like' />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/40031356?v=4" alt="" />
+                    <footer>
+                        <strong>Matheus Santana</strong>
+                        <p>Computer Science Student - UFCG</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt='dislike' />
+                        </button>
+                        <button type="button">
+                            <img src={like} alt='like' />
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div> 
     );
 
 };
